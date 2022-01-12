99a1855b75b148a2bc4f2415880469cc0344be48
Bhartipatel123 <mailtobhartipatel@gmail.com>
2022-01-12 19:06:10 +0530

    SpringBank

---
+++ JavaBackend /SpringBank/src/main/java/com/marticus/AccountAcct.java
@@ -0,0 +1,24 @@
+   package com.marticus;
+
+public class AccountAcct {
+	
+	public Account acct;
+	
+	public Account getAcct() {
+	
+		return acct;
+	}
+	
+	public void setAcct(Account acct) {
+	
+		this.acct = acct;
+	}
+	
+	
+public void AcctDetails() {
+		
+	acct.withDrawMoney();
+		
+	}
+
+}
