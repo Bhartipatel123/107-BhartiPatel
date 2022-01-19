720fe99800fc5a21c75ef7d43c0a597d0ceed04d
Bhartipatel123 <mailtobhartipatel@gmail.com>
2022-01-19 15:10:17 +0530

    React JS

---
+++ React JS/shopping-cart/src/data.js
@@ -0,0 +1,40 @@
+import img1 from './img/iphone13pro.jpeg';
+import img2 from './img/vivo.jpeg';
+import img3 from './img/vivo-iqoo-9-1.jpeg';
+import img4 from './img/oneplus-10-pro-2.jpeg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'iphone 13 pro',
+            desc:'',
+            price: 89000,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Vivo X70 pro plus',
+            desc:'',
+            price: 80000,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'vivo iqoo 9',
+            desc:'',
+            price: 50000,
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'oneplus 10 pro',
+            desc:'',
+            price: 80000,
+        }
+
+    ],
+};
+
+export default data;
