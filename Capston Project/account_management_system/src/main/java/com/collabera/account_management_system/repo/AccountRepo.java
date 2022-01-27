25af6a43b3f6a571c45e8757ffd0816d98087590
Bhartipatel123 <mailtobhartipatel@gmail.com>
2022-01-27 18:24:38 +0530

    delete

--- Capston Project/account_management_system/src/main/java/com/collabera/account_management_system/repo/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.collabera.account_management_system.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.collabera.account_management_system.entity.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Integer> {
-
-}
